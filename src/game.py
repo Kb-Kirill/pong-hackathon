429776b42224d187e054ae32c22d982385eec655
Andrey Bochkov <>
2025-07-12 12:12:26 +0200

    paddle_image.set_alpha

--- src/game.py
+++ src/game.py
@@ -192,6 +192,10 @@ while running:
             # Переводим нормализованные координаты в позицию ракетки
             paddle_pos[0] = int(x * WIDTH - 70)  # Смещение на половину ширины ракетки
             paddle_pos[1] = int(y * HEIGHT - 70)
+            if paddle_pos[1] >= 350:
+                 paddle_image.set_alpha(255)
+            else:
+                 paddle_image.set_alpha(128)
             # Ограничиваем движение ракетки по горизонтали
             paddle_pos[0] = max(0, min(paddle_pos[0], WIDTH - 140))
 
